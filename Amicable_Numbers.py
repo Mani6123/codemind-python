a5a0d4f05e70b4f7e5eaaf4d69d70e81827c947f
manikanta <mbsahadev@gmail.com>
2022-11-03 16:48:25 +0530

    def ami(n):     c=0     for i in range(1,n):         if n%i==0:             c+=i     return c
    a=int(input())
    b=int(input())
    if ami(a)==b and ami(b)==a :
        print('Amicable')
    else:
        print('Not Amicable')

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,13 +1,13 @@
+def ami(n):
+    c=0
+    for i in range(1,n):
+        if n%i==0:
+            c+=i
+    return c
+    
 a=int(input())
 b=int(input())
-a1=b1=0
-for i in range(1,a):
-    if a%i==0:
-        a1+=i
-for j in range(1,b):
-    if b%j==0:
-        b1+=j
-if a==b1 and b==a1:
-    print("Amicable")
+if ami(a)==b and ami(b)==a :
+    print('Amicable')
 else:
-    print("Not Amicable")
+    print('Not Amicable')
